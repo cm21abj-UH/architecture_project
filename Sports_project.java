5f2f5a3c71b79b92b84534238020198de06f5527
cm21abj-UH <114064417+cm21abj-UH@users.noreply.github.com>
2024-01-06 23:46:51 +0000

    Update Sports_project.java
    remembered to add comments with my name and uni info, and altered the code to include project_interface.java

--- Sports_project.java
+++ Sports_project.java
@@ -1,15 +1,18 @@
+/*
+Carter Marshall
+cm21aj@herts.ac.uk
+*/
 package arcitectureproject.sports_project;
 
 public class Sports_project {
     public static void main(String[] args) {
+        project_interface ui = new project_interface();
         competitorInfo compInfo = new competitorInfo();
         management manager = new management();
-        //compInfo.getScoreArray();
-        //compInfo.getCompetitorInfo(args); 
-        //compInfo.getOverallScore(compInfo.Scores);
-        //compInfo.getFullDetails();
-        manager.getFile();
-        manager.updateList(manager.fileName, manager.CompetitorList);
-        manager.makeReport(manager.fileName, manager.CompetitorList); //prints file
+
+        ui.PanelMain();
+        //manager.getFile();
+        //manager.updateList(manager.fileName);
+        //manager.makeReport(manager.fileName); //prints file
     }
 }
