f3b3c087508f40f3faf3dff14e4f02d7ccd33c9f
cm21abj-UH <114064417+cm21abj-UH@users.noreply.github.com>
2024-01-08 23:18:32 +0000

    Update Sports_project.java
    re-enabled several features and added something to call the ui

--- Sports_project.java
+++ Sports_project.java
@@ -6,13 +6,13 @@ package arcitectureproject.sports_project;
 
 public class Sports_project {
     public static void main(String[] args) {
-        project_interface ui = new project_interface();
-        competitorInfo compInfo = new competitorInfo();
+        //project_interface ui = new project_interface();
+        //competitorInfo compInfo = new competitorInfo();
         management manager = new management();
 
-        ui.PanelMain();
-        //manager.getFile();
-        //manager.updateList(manager.fileName);
-        //manager.makeReport(manager.fileName); //prints file
+        manager.promptFile(); //asks for file input
+        manager.updateList(manager.getFile()); //updates the arraylist
+        manager.makeReport(manager.getFile()); //creates file
+        manager.openPanel(); //opens the GUI
     }
 }
