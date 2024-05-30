c2a04df8e5b556dee8fe133277dbabdb84f010f0
cm21abj-UH <114064417+cm21abj-UH@users.noreply.github.com>
2024-05-31 00:31:23 +0100

    Update Sportsproject_revamp.java
    removed several testing/debugging code and added a call to the user interface, initially meant to be in management class, but couldn't get it to work properly due to overflow errors.

--- Sportsproject_revamp.java
+++ Sportsproject_revamp.java
@@ -8,26 +8,14 @@ import java.io.FileNotFoundException;
 
 public class Sportsproject_revamp {
     public static void main(String[] args) throws FileNotFoundException {
+        sportsinterface ui = new sportsinterface();
         manager_class manager = new manager_class();
         footballer footballPlayer = new footballer();
         competitor_list complist = new competitor_list();
-        
-
-        manager.getFile();
-
-        /*!!!everything below is a class test!!!
-        footballPlayer.setAge(22);
-        footballPlayer.setCompNumber(555);
-        footballPlayer.setCountry("UK");
-        footballPlayer.setLevel("Novice");
-        footballPlayer.setName("Cool Name");
-        footballPlayer.getOverallScore();
-        !!!everything above is a class test!!!*/
 
         complist.setCompList();
         complist.setList();
         complist.prntList();
-        footballPlayer.getFullDetails();
-        //manager.outputFile();
+        ui.FrameMain();
     }
 }
