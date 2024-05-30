940a9e61da4f21e2d4d5379eaaa0bd5f2aa15d2e
cm21abj-UH <114064417+cm21abj-UH@users.noreply.github.com>
2024-05-31 00:45:20 +0100

    Update Sportsproject_revamp.java
    calls setFile() again due to a bug.

--- Sportsproject_revamp.java
+++ Sportsproject_revamp.java
@@ -12,10 +12,13 @@ public class Sportsproject_revamp {
         manager_class manager = new manager_class();
         footballer footballPlayer = new footballer();
         competitor_list complist = new competitor_list();
-
+        
+        //note that this program currently asks for the file used twice as inheritence is slightly broken for file reader/report.txt maker
+        manager.setFile();
         complist.setCompList();
         complist.setList();
         complist.prntList();
+        manager.outputFile();
         ui.FrameMain();
     }
 }
